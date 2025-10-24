deeb248426ca578f2b106e79c4606c671f5bc873
christnguema <christnguema15@gmail.com>
2025-10-24 09:33:35 +0200

    Initialization

---
+++ MAUI/BlindAssistant/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "BlindAssistant")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "BlindAssistant.Pages")]
