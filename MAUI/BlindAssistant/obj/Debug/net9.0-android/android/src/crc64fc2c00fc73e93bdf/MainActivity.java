5453494296bee0d9341589e1a28f6d47c113a4d2
christnguema <christnguema15@gmail.com>
2025-10-24 10:44:06 +0200

    Delete obj path in .gitignore

---
+++ MAUI/BlindAssistant/obj/Debug/net9.0-android/android/src/crc64fc2c00fc73e93bdf/MainActivity.java
@@ -0,0 +1,54 @@
+package crc64fc2c00fc73e93bdf;
+
+
+public class MainActivity
+	extends crc6488302ad6e9e4df1a.MauiAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("BlindAssistant.MainActivity, BlindAssistant", MainActivity.class, __md_methods);
+	}
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("BlindAssistant.MainActivity, BlindAssistant", "", this, new java.lang.Object[] {  });
+		}
+	}
+
+	public MainActivity (int p0)
+	{
+		super (p0);
+		if (getClass () == MainActivity.class) {
+			mono.android.TypeManager.Activate ("BlindAssistant.MainActivity, BlindAssistant", "System.Int32, System.Private.CoreLib", this, new java.lang.Object[] { p0 });
+		}
+	}
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
